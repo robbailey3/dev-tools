6b152b550e8707c98cc702affed2edc406e74436
Rob Bailey <rob.bailey3@gmail.com>
2022-03-15 14:31:18 +0000

    build: making port scanner work

--- tcp/tcp.go
+++ tcp/tcp.go
@@ -2,9 +2,18 @@ package tcp
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
 	"net"
+	"sync"
 	"time"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	path        string
+	startPort   int
+	endPort     int
+	concurrency int
 )
 
 var tcp = &cobra.Command{
@@ -15,61 +24,63 @@ var scan = &cobra.Command{
 	Use: "scan",
 	Run: func(cmd *cobra.Command, args []string) {
 		openPorts := PortScan()
-		for _, i := range openPorts {
-			fmt.Println(fmt.Sprintf("Port %d is open", i))
-		}
+		fmt.Printf("Open ports: %v\n", openPorts)
 	},
 }
 
 func Commands() *cobra.Command {
+	tcp.PersistentFlags().StringVarP(&path, "path", "p", "robbailey3.co.uk", "The path to scan")
+	tcp.PersistentFlags().IntVarP(&startPort, "start", "s", 1, "The start port")
+	tcp.PersistentFlags().IntVarP(&endPort, "end", "e", 1024, "The end port")
+	tcp.PersistentFlags().IntVarP(&concurrency, "concurrency", "c", 10, "The number of concurrent connections")
 	tcp.AddCommand(scan)
 
 	return tcp
 }
 
 func PortScan() []int {
-	ports := make(chan int, 100)
+	ports := make(chan int, concurrency)
 	results := make(chan int)
 	var openPorts []int
 
-	//var wg sync.WaitGroup
+	var wg sync.WaitGroup
+
+	wg.Add(endPort - startPort)
+
+	go func() {
+		for i := startPort; i < endPort; i++ {
+			ports <- i
+		}
+		close(ports)
+	}()
 
 	for i := 0; i < cap(ports); i++ {
-		go checkPorts("robbailey3.co.uk", ports, results)
+		go func() {
+			for port := range ports {
+				if checkPort(path, port, &wg) {
+					results <- port
+				}
+			}
+		}()
 	}
 
 	go func() {
-		for i := 75; i < 92; i++ {
-			//wg.Add(1)
-			ports <- i
-		}
+		wg.Wait()
+		close(results)
 	}()
-	for i := range results {
-		openPorts = append(openPorts, i)
+
+	for port := range results {
+		openPorts = append(openPorts, port)
 	}
 
 	return openPorts
 }
 
-func checkPorts(path string, ports chan int, results chan int) {
-	for port := range ports {
-		if checkPort(path, port) {
-			results <- port
-		}
-	}
-}
-
-func checkPort(path string, port int) bool {
+func checkPort(path string, port int, wg *sync.WaitGroup) bool {
+	defer wg.Done()
 	address := fmt.Sprintf("%s:%d", path, port)
-
-	fmt.Println(fmt.Sprintf("Checking port %d", port))
 
 	_, err := net.DialTimeout("tcp", address, 5*time.Second)
 
-	if err != nil {
-		fmt.Println(fmt.Sprintf("Port %d is closed", port))
-		return false
-	}
-	fmt.Println(fmt.Sprintf("Port %d is open", port))
-	return true
+	return err == nil
 }
